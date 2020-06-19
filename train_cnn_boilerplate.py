6a2b8b706a1a86c229681e83a2158a5dbdbc3931
Graham Picard <grahampicard@gmail.com>
2020-06-19 12:55:46 -0400

    add saving capabilities for cnn models

--- train_cnn_boilerplate.py
+++ train_cnn_boilerplate.py
@@ -13,7 +13,7 @@ from torch.utils.data.sampler import SubsetRandomSampler
   
 # user-created files
 from models.cnn_boilerplate import CNN
-from models.cnn_boilerplate import CNN2
+from models.cnn_small import CNN_small
 from source.data_loaders import load_stft_data
 
 
@@ -67,12 +67,12 @@ if __name__ == "__main__":
     parser.add_argument('--batch-size', type=int, default=5, metavar='N', help='input batch size for training (default: 5)')
     parser.add_argument('--test-batch-size', type=int, default=5, metavar='N', help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=1, metavar='N', help='number of epochs to train (default: 5)')
-    parser.add_argument('--lr', type=float, default=0.5, metavar='LR', help='learning rate (default: 0.01)')
+    parser.add_argument('--lr', type=float, default=0.1, metavar='LR', help='learning rate (default: 0.01)')
     parser.add_argument('--momentum', type=float, default=0.1, metavar='M', help='SGD momentum (default: 0.5)')
     parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False, help='For Saving the current Model')
+    parser.add_argument('--save-model', action='store_true', default=True, help='For Saving the current Model')
     args = parser.parse_args()
     kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -87,8 +87,11 @@ if __name__ == "__main__":
     test_loader = DataLoader(test_dataset, batch_size=args.test_batch_size, **kwargs)
 
     # Instantiate model
-    model = CNN2().to(device)    
+    model = CNN_small().to(device)    
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
         train(model, optimizer, train_loader, device, epoch, args)
+
+    if (args.save_model):
+        torch.save(model.state_dict(),"./data/processed/cnn-boilerplate.pt")
